a9b32375c94ada0a4d9081f0e90b414650357f81
Shadow <hungnh@binhhoang.com>
2018-06-21 22:36:55 +0700

    test bug 2

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello Dammio!');
-}).listen(3000);
+}).listen(3000, '116.93.97.239');
